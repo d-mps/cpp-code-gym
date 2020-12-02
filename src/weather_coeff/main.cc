9b75339b60838b3565e992985441e9b2186ca6c0
Diana Spencer <diana.ponce.spencer@gmail.com>
2020-12-02 10:49:22 +0100

    remove bin

--- src/weather_coeff/main.cc
+++ src/weather_coeff/main.cc
@@ -1,26 +1,55 @@
+/**
+ * Description: Write a function that accepts a beginning date and time, 
+ * and an ending date and time. Inclusive of those dates and times,
+ * return the coefficient of the slope of the barometric pressure.
+ * 
+ * Given: Text files of weather data for years 2012 through 2015. 
+ * Each observation includes, respectively:
+ *     date, time, temp, barometric pressure, dew point, humidity, wind dir, wind gust, wind speed
+ * 
+ * @author Diana Spencer
+ * 
+*/
+
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <sstream>
-#include <string>
-#include <gtest/gtest.h>
+#include <numeric>
 
 using namespace std;
 
 
+// grab selection of dates
+int * findDatesIndices(string beginDate, string endDate, const vector<string> &date) {
+    static int *indices[2];
+}
+
+
 // need to define a function header here
-int barometricCoefficient(string beginDate, string endDate, vector<string>* dates, vector<string>* times, vector<int>* pressures) {
+int barometricCoefficient( const vector<string> &timestamp, int *baroptr) {
+    cout << baroptr << endl;
     return 1;
+}
+// Vector is passed by reference and cannot be changed by this function.
+float baroTest(const vector<float> &vec) {
+    // for (int i=0; i < vec.size(); i++) {
+    //     cout << vec[i] << endl;
+    // }
+    // Rounding error here
+    float total;
+    total = accumulate(vec.begin(), vec.end(), 0);
+    return total;
 }
 
 
 int main() {
     // Read data from file.
-    ifstream file ("../data/weather/2015.txt");
+    ifstream file ("../../data/weather/example.txt");
 
     // TODO: test the file opens and does not fail and fails
     if (!file.is_open()) {
-        cout << "Error: couldn't open file." << endl;
+        cout << "Error: Hey, you couldn't open this file." << endl;
         exit(-2);
     }
 
@@ -42,10 +71,13 @@ int main() {
         barometrics.push_back(barometricPress);
     }
     
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 4; i++) {
         cout << i << " " << barometrics[i] << endl;
     }
 
+    float total;
+    total = baroTest(barometrics);
+    printf("Total %f\n", total);
     file.close();
 
     return 0;
